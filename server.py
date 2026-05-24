2d5153e21126bd572f2ddcdd68a6971ce26620e7
matk91589-dev <matk91589@gmail.com>
2026-05-24 16:41:37 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import datetime, time, sqlite3
+
+app = FastAPI()
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS my_app(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        text TEXT,
+        create_at INTEGER
+    )
+""")
+
+class NoteRequest(BaseModel):
+    text: str
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/ping")
+def ping():
+    return {"status": "ok", "message": "Сервер жив!"}
+
+@app.get("/api/time")
+def get_time():
+    return {"time": datetime.datetime.now().isoformat()}
+
+@app.post("/api/notes")
+def save_notes(note: NoteRequest):
+    text = note.text
+    cursor.execute("INSERT INTO my_app (text, create_at) VALUES (?, ?)", (text, int(time.time())))
+    conn.commit()
+    return {"status": "ok", "message": "Заметка сохранена"}
